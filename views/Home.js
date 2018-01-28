334433363e10a6266f77835d430dec58a6daabe5
Steven Lao <lao.steven92@gmail.com>
2018-01-27 23:48:50 -0500

    Tabbar

--- views/Home.js
+++ views/Home.js
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { Component } from "react";
 import {
+  AppRegistry,
   Text,
   View,
   Image,
@@ -9,14 +10,21 @@ import {
   TextInput,
   StyleSheet
 } from "react-native";
-import { Header, Searchbar, SearchBar } from "react-native-elements";
-import { StackNavigator } from 'react-navigation'; 
+import {
+  Header,
+  SearchBar,
+  Tab,
+  Icon
+} from "react-native-elements";
+import { StackNavigator } from 'react-navigation';
 import { Constants } from 'expo';
+import Tabs from 'react-native-tabs';
+import { TabViewAnimated, TabBar, SceneMap } from 'react-native-tab-view';
 
 const searchStyle = StyleSheet.create({
   header: {
     paddingTop: Constants.statusBarHeight,
-    backgroundColor: '#e1e8ee',
+    backgroundColor: '#e1e8ee'
   },
 });
 
@@ -26,17 +34,26 @@ const { width, height } = Dimensions.get("window");
 
 export default class HomeScreen extends React.Component {
   static navigationOptions = {
-    title: "Home",
-    header: null
+    title: "Home"
   };
 
   constructor(props) {
     super(props);
+    this.state =
+      {
+        selectedTab: 'home'
+      }
   }
 
+  changeTab(selectedTab) {
+    this.setState({ selectedTab })
+  }
+
+
   render() {
+    const { selectedTab } = this.state
     return (
-      <View>
+      <View style={styles.container}>
         <FlatList
           data={[
             {
@@ -68,7 +85,23 @@ export default class HomeScreen extends React.Component {
             </View>
           )}
         />
+
+        <Tabs selected={this.state.page} style={{ backgroundColor: 'white' }}>
+          <View>
+            <Icon name="gift" type="font-awesome" containerStyle={{ justifyContent: 'center', alignItems: 'center', marginBottom: 2 }} color={'#ff880a'} />
+            <Text name='Donations' style={{color: '#ff880a', fontSize: 11}}>Donations</Text>
+          </View>
+          <View>
+            <Icon name="ticket" type="font-awesome" containerStyle={{ justifyContent: 'center', alignItems: 'center', marginBottom: 2 }} color={'#5e6977'} />
+            <Text name='Bids' style={{fontSize: 11}}>Bids</Text>
+          </View>
+          <View>
+            <Icon name="comments" type="font-awesome" containerStyle={{ justifyContent: 'center', alignItems: 'center', marginBottom: 2 }} color={'#5e6977'} />
+            <Text name='Notifications' style={{fontSize: 11}}>Notifications</Text>
+          </View>
+        </Tabs>
       </View>
+
     );
   }
 
@@ -79,6 +112,19 @@ export default class HomeScreen extends React.Component {
 
 HomeScreen.navigationOptions = {
   header: <View style={searchStyle.header}>
-    <SearchBar lightTheme/>
+    <View>
+      <SearchBar lightTheme
+        placeholder='Search bids' />
+    </View>
   </View>
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+    paddingTop: 5
+  }
+});
